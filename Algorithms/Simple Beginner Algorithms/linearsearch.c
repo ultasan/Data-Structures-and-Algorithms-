2b3988e9fcad8f350c22342e4d6b1fd5a5df940f
ultasan <93845673+ultasan@users.noreply.github.com>
2022-11-20 11:38:33 -0800

    Create linearsearch.c

---
+++ Algorithms/Simple Beginner Algorithms/linearsearch.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main(){
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int i;
+        int target = 5;
+        for(i=0;i<9;i++){
+                if(arr[i] == target){
+                        printf("Reached target number %d\n", arr[i]);
+}
+}
+}
