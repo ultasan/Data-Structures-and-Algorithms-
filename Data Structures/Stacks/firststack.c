b85b147b75b166e7c4caed8f544f0e6b5ebcc4a5
ultasan <93845673+ultasan@users.noreply.github.com>
2022-11-26 18:09:20 -0800

    Add files via upload

---
+++ Data Structures/Stacks/firststack.c
@@ -0,0 +1,48 @@
+# include <stdio.h>
+# define MAX_SIZE 101
+
+int A[MAX_SIZE];
+int top = -1;
+
+void Push(int x){
+	top++;
+	if(top == MAX_SIZE-1){
+		printf("Error: Stack Overflow\n");
+		return;
+}
+	A[top] = x;
+};
+
+void Pop(){
+	if(top == -1){
+		printf("Error: no element to pop\n");
+		return;
+}
+	top--;
+};
+
+int Top(){
+	return A[top];
+};
+
+void Print(){
+	int i;
+	printf("Stack: ");
+	for(i=0; i<=top; i++){
+		printf("%d ",A[i]);
+}
+	printf("\n");
+};
+
+int main(){
+	Push(2);
+	Print();
+	Push(5);
+	Print();
+	Push(10);
+	Print();
+	Pop();
+	Print();
+	Push(12);
+	Print();
+};
